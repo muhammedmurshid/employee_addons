62348b0705ccbd1737a51e2a1d27557803594af3
Murshid <muhsink00000@gmail.com>
2023-11-06 14:28:14 +0530

    added employee id for employees

--- models/employee_private_number.py
+++ models/employee_private_number.py
@@ -5,6 +5,7 @@ class EmployeePrivateNumber(models.Model):
     _inherit = 'hr.employee'
 
     private_number = fields.Char('Alternate Number')
+    employee_id = fields.Char('Employee ID')
 
 
 class HRLeavesActionCustomization(models.Model):
