baa622de8c644e6ffd72241c9a47bf24f80d26ae
Murshid <muhsink00000@gmail.com>
2023-11-22 11:21:28 +0530

    add new branch

--- models/employee_private_number.py
+++ models/employee_private_number.py
@@ -9,12 +9,10 @@ class EmployeePrivateNumber(models.Model):
     branch = fields.Selection([('corporate_office', 'Corporate Office'), ('cochin_campus', 'Cochin Campus'),
                                ('kottayam_campus', 'Kottayam Campus'), ('calicut_campus', 'Calicut Campus'),
                                ('malappuram_campus', 'Malappuram Campus'), ('trivandrum_campus', 'Trivandrum Campus'),
-                               ('palakkad_campus', 'Palakkad Campus'), ('dubai_campus', 'Dubai Campus')],
+                               ('palakkad_campus', 'Palakkad Campus'), ('dubai_campus', 'Dubai Campus'),
+                               ('jk_shah_classes', 'JK Shah Classes')],
                               string='Branch')
 
 
 class HRLeavesActionCustomization(models.Model):
     _inherit = 'hr.leave'
-
-
-
