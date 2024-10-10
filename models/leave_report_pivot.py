b0cd4f31e3cbe1c3c2f5760e1b4aebe0748fe586
muhammedmurshid <muhsink00000@gmail.com>
2024-10-10 12:02:48 +0530

    print

--- models/leave_report_pivot.py
+++ models/leave_report_pivot.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import datetime
 
 
 class LeaveReportPivotCustom(models.Model):
@@ -14,23 +15,35 @@ class LeaveReportPivotCustom(models.Model):
 
     def action_duplicate_accounts(self):
 
-        employees = self.env['hr.employee'].search([],order='id asc')
+        current_year = datetime.now().year
+        print(current_year, 'year')
+        employees = self.env['hr.employee'].search([], order='id asc')
         leave_types = self.env['hr.leave.type'].search([], order='id asc')
+        print(current_year, 'year')
+        # Clear previous pivot report data
         self.env['leave.report.pivot'].search([]).unlink()
-        # for rec in self_rec:
-        #     rec.unlink()
-        emp_leave_data = leave_types.get_employees_days(employees.ids)
-        for employee in employees:
-            # emp_ids.append(employee.id)
-            for leave_type_id in leave_types:
 
-                self.env['leave.report.pivot'].create({
-                    'employee_id': employee.id,
-                    'remaining_leaves': emp_leave_data[employee.id][leave_type_id.id]['virtual_remaining_leaves'],
-                    'leave_type': leave_type_id.name,
-                    'max_leaves': emp_leave_data[employee.id][leave_type_id.id]['max_leaves'],
-                    'taken_leaves': emp_leave_data[employee.id][leave_type_id.id]['virtual_leaves_taken']
-                })
+        # Get employee leave data for the current year
+        emp_leave_data = leave_types.get_employees_days(employees.ids)
+
+        for employee in employees:
+            for leave_type_id in leave_types:
+                # Check if there are leaves for the current year
+                leaves = self.env['hr.leave'].search([
+                    ('employee_id', '=', employee.id),
+                    ('holiday_status_id', '=', leave_type_id.id),
+                    ('date_from', '>=', f'{current_year}-01-01'),
+                    ('date_to', '<=', f'{current_year}-12-31'),
+                ])
+
+                if leaves:  # Only process if leaves exist for the current year
+                    self.env['leave.report.pivot'].create({
+                        'employee_id': employee.id,
+                        'remaining_leaves': emp_leave_data[employee.id][leave_type_id.id]['virtual_remaining_leaves'],
+                        'leave_type': leave_type_id.name,
+                        'max_leaves': emp_leave_data[employee.id][leave_type_id.id]['max_leaves'],
+                        'taken_leaves': emp_leave_data[employee.id][leave_type_id.id]['virtual_leaves_taken']
+                    })
         # self.env['leave.report.pivot'].create({
         #     'employee_id': self.employee_id.id,
         #     'leave_type': self.leave_type.id
